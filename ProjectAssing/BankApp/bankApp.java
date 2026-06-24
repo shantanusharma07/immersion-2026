130c26419d96c19f93db20d2e28412200ae61413
shantanusharma07 <shantanu2004jarvis@gmail.com>
2026-06-24 09:12:21 +0530

    Updated BankApp

--- ProjectAssing/BankApp/bankApp.java
+++ ProjectAssing/BankApp/bankApp.java
@@ -1,8 +1,30 @@
+
+
 package ProjectAssing.BankApp;
 
-public class bankApp {
+public class bankApp{
     public static void main(String[] args) {
-        System.out.println("hello");
-    }
 
+        BankAccount account1 =
+                new BankAccount(101, "Shantanu Sharma",
+                        "Savings", 10000);
+
+        BankAccount account2 =
+                new BankAccount(102, "Rahul Kumar", "Current", 15000);
+
+        account1.displayDetails();
+        account2.displayDetails();
+
+        account1.deposit(5000);
+
+        try {
+            account2.withdraw(3000);
+            account1.withdraw(15000);
+        } catch (insufficientBalanceException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        account1.displayDetails();
+        account2.displayDetails();
+    }
 }
